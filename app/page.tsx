ca123ed619c9ca3d4d12200e966234a5abc02e6d
simuratubasa <ktc25a31g0006@edu.kyoto-tech.ac.jp>
2025-11-15 11:28:09 +0900

    feat: update styles and structure for the homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import  Styles from "./page.module.css" 
+import Image from "next/image"
+
 export default function Home() {
-  const name = "世界";
   return (
-  <div>
-    <h1>テクノロジーの力で{name}をかえる</h1>
-    <p>私たちは市場をグローバルスティックカンパニーです。</p>
-  </div>
+    <section className={Styles.top}>
+      <div>
+      <h1 className={Styles.title}>テクノロジーの力で世界をかえる</h1>
+      <p className={Styles.description}>
+        私たちは市場をグローバルスティックカンパニーです。
+      </p>
+    </div>
+    <Image
+      className={Styles.bigImage}
+      src="/img-mv.jpg"
+      alt=""
+      width={4000}
+      height={1200}
+    />
+    <img className={Styles.image} src="/img-mv.jpg" alt="" />
+  </section>
   );
 }
