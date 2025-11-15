3980248e2ec9acf10416ca4bb1a0703e83ec9ef6
simuratubasa <ktc25a31g0006@edu.kyoto-tech.ac.jp>
2025-11-15 12:38:11 +0900

    教科書３

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,49 @@
 import  Styles from "./page.module.css" 
 import Image from "next/image"
 
+type News = {
+  id:string;
+  title:string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+}
+
+const data: {
+  contents: News[] } = {
+    "contents": [
+      {
+       "id": "1",
+       "title": "渋谷にオフィスを移転しました",
+       "category": {
+          name: "更新情報",
+        },
+        publishedAt: "2023・05/19",
+        createdAt: "2023/05/19",
+      },
+      {
+        id: "2" ,
+        title: "当社CEOが業界リーダートップ30に選出されました",
+        category: {
+          name: "更新情報",
+        },
+        publishedAt: "2023・05/19",
+        createdAt: "2023/05/19",
+      },
+      {
+        id: "3",
+        title: "テストの記事です",
+          category: {
+            name: "更新情報",
+          },
+          publishedAt: "2023・04/19",
+          createdAt: "2023/04/19",
+      },
+    ],
+  };
+
 export default function Home() {
   return (
     <section className={Styles.top}>
